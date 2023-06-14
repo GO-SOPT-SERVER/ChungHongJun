1b9a103e5d4b2755e8b17f6df3cd838737da0dd7
Hong Jun <rhkgkrwkhj@naver.com>
2023-06-14 20:26:27 +0900

    Week#6

---
+++ src/main/java/sopt/org/fourthSeminar/controller/dto/request/BoardImageListRequestDto.java
@@ -0,0 +1,23 @@
+package sopt.org.fourthSeminar.controller.dto.request;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter // ?
+public class BoardImageListRequestDto {
+    private List<MultipartFile> boardImages;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    private Boolean isPublic;
+}
